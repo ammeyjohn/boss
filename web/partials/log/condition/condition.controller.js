1c64dd9289762f2d2a60f62b1743e644bc409d9f
yuanjie <yuanjie@shanghai3h.com>
2017-01-14 15:53:32 +0800

    查询条件页面增加搜索日期快捷按钮

--- web/partials/log/condition/condition.controller.js
+++ web/partials/log/condition/condition.controller.js
@@ -11,8 +11,18 @@ define([
 
     function LogConditionCtrl($scope, $rootScope, $cookies, prjApi) {
 
+        var DATE_FORMAT = 'YYYY-MM-DD';
+
         // 用户权限
         var credential = $cookies.getObject('credential');
+
+        var default_condition = {
+            logType: 2,
+            projectCode: '',
+            user: credential.account,
+            logStartTime: moment().subtract(7, 'days').format(DATE_FORMAT),
+            logEndTime: moment().format(DATE_FORMAT)
+        }
 
         // 常用项目列表
         $scope.projects = [];
@@ -21,13 +31,7 @@ define([
         $scope.logTypes = settings.logTypes;
 
         // 条件对象
-        $scope.condition = {
-            logType: 2,
-            projectCode: '',
-            user: credential.account,
-            logStartTime: moment().subtract(7, 'days').format('YYYY-MM-DD'),
-            logEndTime: moment().format('YYYY-MM-DD')
-        };
+        $scope.condition = _.cloneDeep(default_condition);
 
         // 加载常用项目编号列表
         prjApi.getProjectByUser({
@@ -35,6 +39,52 @@ define([
         }).$promise.then(function(projects) {
             $scope.projects = projects.data;
         });
+
+        $scope.quickSearch = function(timeSpan) {
+            var st, et;
+            switch (timeSpan) {
+                case 'Today':
+                    st = moment();
+                    et = moment().add(1, 'days');
+                    break;
+                case 'Yesterday':
+                    st = moment().subtract(1, 'days');
+                    et = moment();
+                    break;
+                case 'CurWeek':
+                    st = moment().day(0).add(1, 'days');
+                    et = moment().day(7);
+                    break;
+                case 'PreWeek':
+                    st = moment().day(0).subtract(6, 'days');
+                    et = moment().day(7).subtract(7, 'days');
+                    break;
+                case 'CurMonth':
+                    st = moment().startOf('month');
+                    et = moment().endOf('month');
+                    break;
+                case 'PreMonth':
+                    st = moment().startOf('month').subtract(1, 'months');
+                    et = moment().endOf('month').subtract(1, 'months');
+                    break;
+                case 'CurYear':
+                    st = moment().startOf('year');
+                    et = moment().endOf('year');
+                    break;
+                case 'PreYear':
+                    st = moment().startOf('year').subtract(1, 'years');
+                    et = moment().endOf('year').subtract(1, 'years');
+                    break;
+            }
+
+            $scope.condition.logStartTime = st.format(DATE_FORMAT);
+            $scope.condition.logEndTime = et.format(DATE_FORMAT);
+            $scope.search();
+        }
+
+        $scope.clear = function() {
+            $scope.condition = _.cloneDeep(default_condition);
+        }
 
         $scope.search = function() {
             $rootScope.$broadcast('BOSS_SEARCH', $scope.condition);
