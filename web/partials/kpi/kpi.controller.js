740092c60184bda88b6206e6162bed8965e33bf3
Patrick <ammeyjohn@gmail.com>
2017-02-07 22:17:42 +0800

    部分完成人员分组统计

--- web/partials/kpi/kpi.controller.js
+++ web/partials/kpi/kpi.controller.js
@@ -15,39 +15,6 @@ define([
 
     function KpiCtrl($scope, $rootScope, $q, $cookies, userApi, deptApi, prjApi, logApi) {
 
-        // // 加载用户列表
-        // $scope.users = null;
-        // $scope.nodes = null;
-
-        // $q.all([
-        //     userApi.get().$promise,
-        //     deptApi.get().$promise
-        // ]).then(function(results) {
-
-        //     var nodes = [];
-
-        //     _.each(results[1].data, function(dept) {
-        //         nodes.push({
-        //             id: 'D' + dept.id,
-        //             pId: dept.parent === null ? 'ROOT' : 'D' + dept.parent,
-        //             name: dept.name,
-        //             open: true
-        //         });
-        //     });
-
-        //     var users = results[0].data;
-        //     _.each(users, function(user) {
-        //         nodes.push({
-        //             id: user.id,
-        //             pId: 'D' + user.department,
-        //             name: user.name
-        //         });
-        //     });
-
-        //     $scope.nodes = _.orderBy(nodes, ['id'], ['asc']);
-
-        // });
-
         $scope.dashboard = {
             taskTime: 0,
             overTime: 0,
@@ -55,7 +22,7 @@ define([
             projectCount: 0
         }
 
-        $scope.option = {
+        $scope.option = [{
             grid: {
                 top: 10,
                 left: 40,
@@ -94,7 +61,42 @@ define([
                 type: 'bar',
                 data: null
             }]
-        };
+        }, {
+            grid: {
+                top: 10,
+                left: 40,
+                right: 30
+            },
+            tooltip: {
+                formatter: function(params, ticket, callback) {
+                    var project = sumOfProjectGroup[params.name];
+                    var format = params.name + ' ' + project.projectName;
+                    format += '<br />';
+                    format += '工时: ' + params.value.toFixed(2) + ' 人日';
+                    return format;
+                }
+            },
+            xAxis: {
+                type: 'category',
+                data: null,
+                axisLabel: {
+                    interval: 0
+                }
+            },
+            yAxis: {
+                type: 'value',
+                name: '工单统计（人日）',
+                min: 0,
+                axisLabel: {
+                    interval: 10
+                }
+            },
+            series: [{
+                name: 'taskTime',
+                type: 'bar',
+                data: null
+            }]
+        }];
 
 
         var DATE_FORMAT = 'YYYY-MM-DD';
@@ -114,15 +116,22 @@ define([
 
         logApi.queryLogs(condition).$promise.then(function(logs) {
 
-            // 工时统计            
+            // 总工时统计            
             $scope.dashboard.taskTime = _.sumBy(logs.data, 'workTime') / 480;
 
-            // 项目统计
-            var projectGroup = _.groupBy(logs.data, 'projectCode');
-            $scope.dashboard.projectCount = _.keys(projectGroup).length;
+            // 项目分组统计
+            processProjectGroup(logs);
 
-            // 统计各项目工时
-            _.reduce(projectGroup, function(result, log, key) {
+            // 人员分组统计
+            processUserGroup(logs);
+        });
+
+        var processProjectGroup = function(logs) {
+
+            var logGroupByProject = _.groupBy(logs.data, 'projectCode');
+            $scope.dashboard.projectCount = _.keys(logGroupByProject).length;
+
+            _.reduce(logGroupByProject, function(result, log, key) {
                 result[key] = {
                     taskTime: _.sumBy(log, 'workTime') / 480,
                     projectCode: log[0].projectCode,
@@ -131,20 +140,43 @@ define([
                 return result;
             }, sumOfProjectGroup);
             var orderedProjectGroup = _.orderBy(_.values(sumOfProjectGroup), ['taskTime'], ['desc']);
-            $scope.option.xAxis.data =
+            $scope.option[0].xAxis.data =
                 _.flatMap(orderedProjectGroup, function(d) {
                     return d.projectCode
                 });
-            $scope.option.series[0].data =
+            $scope.option[0].series[0].data =
                 _.flatMap(orderedProjectGroup, function(d) {
                     return d.taskTime
                 });
 
-            // 项目工期Top5
-            $scope.top5Projects = _.take(orderedProjectGroup, 10);
+            // 项目工期Top10
+            $scope.top10Projects = _.take(orderedProjectGroup, 10);
+        }
 
-        });
+        var processUserGroup = function(logs) {
+            var logGroupByUser = _.groupBy(logs.data, 'userName');
+            var sumOfUserGroup =
+                _.reduce(logGroupByUser, function(result, log, key) {
+                    result.push({
+                        userName: log[0].userName,
+                        taskTime: _.sumBy(log, 'workTime') / 480
+                    });
+                    return result;
+                }, []);
+            var orderedUserGroup = _.orderBy(sumOfUserGroup, ['taskTime'], ['desc']);
+            $scope.option[1].xAxis.data =
+                _.flatMap(orderedUserGroup, function(d) {
+                    return d.userName
+                });
+            $scope.option[1].series[0].data =
+                _.flatMap(orderedUserGroup, function(d) {
+                    return d.taskTime
+                });
 
+            // 人员工期Top10
+            $scope.top10Users = _.take(orderedUserGroup, 5);
+            $scope.last10Users = _.takeRight(orderedUserGroup, 5);
+        }
     }
 
     kpiModule.controller('KpiCtrl', [
