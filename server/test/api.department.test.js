8df5a927946761d780b4c6014e74b53bb37f35d9
yuanjie <yuanjie@shanghai3h.com>
2017-10-16 17:41:42 +0800

    [TEST] 尝试对server端进行测试；

---
+++ server/test/api.department.test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var deptApi = require('../api/department.js');
+
+describe('API', function() {
+    describe('Department', function() {
+        describe('addDeparment', function() {
+            it('Should return the new department object with integer id.', function() {
+                var DEPT = {
+
+                };
+                //var new_dept = deptApi.addDeparment()
+                assert.notEqual();
+            });
+        });
+    });
+});
